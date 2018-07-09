9538ac3cd93b476efe3094c05601bad6a1e231f5
Tommy <tommyputranto1@gmail.com>
2018-07-09 14:14:25 +0700

    commit

---
+++ natural-Module/BaseFramework/BaseFramework/BaseFramework.h
@@ -0,0 +1,19 @@
+//
+//  BaseFramework.h
+//  BaseFramework
+//
+//  Created by BRI on 09/07/18.
+//  Copyright © 2018 Tommy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BaseFramework.
+FOUNDATION_EXPORT double BaseFrameworkVersionNumber;
+
+//! Project version string for BaseFramework.
+FOUNDATION_EXPORT const unsigned char BaseFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseFramework/PublicHeader.h>
+
+
